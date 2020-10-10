1d4d0d579bf0769f2fe8f6f0c9cd89bf4fb2491e
elliot.redhead <elliot.redhead@gmail.com>
2020-10-10 15:46:33 +0100

    Fetching data & json-server testing.

--- src/components/fetch.js
+++ src/components/fetch.js
@@ -4,16 +4,35 @@ const Test = () => {
 
   const [fetchData, setFetchData] = useState();
   
-  fetch("https://api.github.com/users/elliotredhead/repos")
+  fetch("https://api.github.com/repos/ElliotRedhead/ReactPortfolio/git/commits")
     .then(res => res.json())
     .then(
       (results) => {
         setFetchData(results);
       }
     );
+    
+  // const writeData = () => {
+  //   fetch("http://localhost:8080/commits", {
+  //     "body": JSON.stringify(fetchData),
+  //     "headers": {
+  //       "Accept": "application/json",
+  //       "Content-Type": "application/json"
+  //     },
+  //     "method": "PUT"
+  //   });
+  // };
+
+
+
+  
+
 
   return (
-    <div>{JSON.stringify(fetchData)}</div>
+    <>
+      <div>{JSON.stringify(fetchData)}</div>
+      {/* <button onClick={writeData}>Push Me</button> */}
+    </>
   );
 
 };

35ead06d60af3b1d334db22230713edc2e8d07d3
ElliotRedhead <elliot.redhead@gmail.com>
2020-10-12 00:49:56 +0100

    Isolated selected repository branch.

--- src/components/fetch.js
+++ src/components/fetch.js
@@ -1,101 +1,66 @@
 import React, {useState} from "react";
 
-const Test = () => {
+// Get list of repos for a user: https://api.github.com/users/ElliotRedhead/repos?per_page=100
+// Get list of branches for a repo: https://api.github.com/repos/ElliotRedhead/ReactPortfolio/commits?per_page=100
 
-  const [fetchData, setFetchData] = useState();
-  let reposAvailable = false;
+const GitCommits = () => {
 
-  if (fetchData){
-    // var commitsDisplay = fetchData.forEach((data, index) => <li key={index}>{data}</li>);
-    var commitsDisplay = <p>{JSON.stringify(fetchData)}</p>;
-  }
-  const [externalInputText, setExternalInputText] = useState("https://api.github.com/users/ElliotRedhead/repos");
-  const [internalInputText, setInternalInputText] = useState("http://localhost:8080/commitdetail");
-  const [githubUserInputText, setGithubUserInputText] = useState("ElliotRedhead");
+  const [repoOptions, setRepoOptions] = useState();
+  const [selectedRepo, setSelectedRepo] = useState();
+
+  const [branchOptions, setBranchOptions] = useState();
+  const [selectedBranch, setSelectedBranch] = useState();
+
+  const [githubUserInput, setGithubUserInput] = useState("ElliotRedhead");
   // const [inputText, setInputText] = useState("http://localhost:8080/commits");
 
-  const externalFetch = () => {
-  
-    fetch(externalInputText)
-      .then(response => response.json())
-      .then(
-        (results) => {
-          console.log(results[0]);
-          setFetchData(results[0]);
-        }
-      );
-  };
-
-  const internalFetch = () => {
-  
-    fetch(internalInputText)
-      .then(response => response.json())
-      .then(
-        (results) => {
-          console.log(results);
-        }
-      );
-  };
 
   const githubUserFetch = () => {
-      
-    fetch(`https://api.github.com/users/${githubUserInputText}/repos`)
+    fetch(`https://api.github.com/users/${githubUserInput}/repos`)
       .then(response => response.json())
       .then(
         (results) => {
           console.log(results);
-          results.map((data, index) => {
-            console.log(data.name);
-            console.log(index);
-          });
-
-          setFetchData(results);
-          reposAvailable = true;
-          console.log(reposAvailable);
+          setRepoOptions(results);
         }
       );
+  };
 
+  const repoBranchFetch = () => {
+    fetch(`https://api.github.com/repos/${githubUserInput}/${selectedRepo}/branches?per_page=100`)
+      .then(response => response.json())
+      .then(
+        (results) => {
+          console.log(results);
+          setBranchOptions(results);
+        }
+      );
   };
-    
-  const writeData = () => {
-    fetch("http://localhost:8080/repos", {
-      "body": JSON.stringify(fetchData),
-      "headers": {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      "method": "PUT"
+
+  const branchCommitFetch = () => {
+    branchOptions.map((data, index) => {
+      if (data.name == selectedBranch){
+        console.log(selectedBranch);
+        console.log(data.commit.sha);
+      }
     });
+    // fetch(`https://api.github.com/repos/${githubUserInput}/${selectedRepo}/commits?per_page=100&sha=4763742bd53530916f92efe164909aafe50f6624`)
+    //   .then(response => response.json())
+    //   .then(
+    //     (results) => {
+    //       console.log(results);
+    // setBranchOptions(results);
   };
-  
+  //     );
+  // };
+
   return (
     <>
       <div>
-        <ul>
-          <li>Get list of repos for a user: https://api.github.com/users/ElliotRedhead/repos?per_page=100</li>
-          <li>Get list of branches for a repo: https://api.github.com/repos/ElliotRedhead/ReactPortfolio/commits?per_page=100</li>
-        </ul>
-      </div>
-      <div>
         <input
           type="text"
-          value={externalInputText}
-          onChange={event => setExternalInputText(event.target.value)}
-        />
-        <button onClick={externalFetch}>External Fetch</button>
-        <input
-          type="text"
-          value={internalInputText}
-          onChange={event => setInternalInputText(event.target.value)}
-        />
-        <button onClick={internalFetch}>Internal Fetch</button>
-        <button onClick={writeData}>Write Data</button>
-      </div>
-      <div>
-        <input
-          type="text"
-          value={githubUserInputText}
-          onChange={event => setGithubUserInputText(event.target.value)}
+          value={githubUserInput}
+          onChange={event => setGithubUserInput(event.target.value)}
         />
         <button onClick={githubUserFetch}>
         Fetch Repos for User
@@ -103,28 +68,45 @@ const Test = () => {
       </div>
 
       <div>
-        {fetchData ?
-          <select
-            onChange={event => console.log(event.target.value)}>
-            {fetchData.map((data,index) => (
-              <option key={index}>{data.name}</option>
-            ))
-            }
-          </select>
+        {repoOptions ?
+          <>
+            <select
+              onChange={event => setSelectedRepo(event.target.value)}>
+              <option default disabled>None</option>
+              {repoOptions.map((data,index) => (
+                <option key={index}>{data.name}</option>
+              ))
+              }
+            </select>
+            <button onClick={repoBranchFetch}>
+              Fetch Branches for Selected Repo
+            </button>
+          </>
           : null
         }
-
       </div>
 
+      <div>
+        {branchOptions ?
+          <>
+            <select
+              onChange={event => setSelectedBranch(event.target.value)}>
+              <option default disabled>None</option>
+              {branchOptions.map((data,index) => (
+                <option key={index}>{data.name}</option>
+              ))}
+            </select>
+            <button onClick={branchCommitFetch}>
+              Fetch Commits for Selected Branch
+            </button>
+          </>
+          : null
+        }
+      </div>
 
-      {/* <div>
-        <ol>
-          {commitsDisplay}
-        </ol>
-      </div> */}
     </>
   );
 
 };
 
-export default Test;
+export default GitCommits;

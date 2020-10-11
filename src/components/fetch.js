f917b2bd27f6ae017798b199766727b8284b2a99
ElliotRedhead <elliot.redhead@gmail.com>
2020-10-11 19:32:46 +0100

    Rendered a dropdown list of repos on user submit.

--- src/components/fetch.js
+++ src/components/fetch.js
@@ -3,14 +3,15 @@ import React, {useState} from "react";
 const Test = () => {
 
   const [fetchData, setFetchData] = useState();
+  let reposAvailable = false;
 
   if (fetchData){
     // var commitsDisplay = fetchData.forEach((data, index) => <li key={index}>{data}</li>);
     var commitsDisplay = <p>{JSON.stringify(fetchData)}</p>;
-    console.log(commitsDisplay);
   }
-  const [externalInputText, setExternalInputText] = useState("https://api.github.com/repos/ElliotRedhead/ReactPortfolio/commits?sha=master&per_page=100");
-  const [internalInputText, setInternalInputText] = useState("http://localhost:8081/commitdetail");
+  const [externalInputText, setExternalInputText] = useState("https://api.github.com/users/ElliotRedhead/repos");
+  const [internalInputText, setInternalInputText] = useState("http://localhost:8080/commitdetail");
+  const [githubUserInputText, setGithubUserInputText] = useState("ElliotRedhead");
   // const [inputText, setInputText] = useState("http://localhost:8080/commits");
 
   const externalFetch = () => {
@@ -19,14 +20,8 @@ const Test = () => {
       .then(response => response.json())
       .then(
         (results) => {
-          console.log(results);
-          let commitDetails = {};
-          results.forEach((data, index) => {
-            console.log(data.commit);
-            // data.commit.message
-            commitDetails[index]={[data.commit.message] : data.commit};
-          });
-          setFetchData(commitDetails);
+          console.log(results[0]);
+          setFetchData(results[0]);
         }
       );
   };
@@ -41,9 +36,29 @@ const Test = () => {
         }
       );
   };
+
+  const githubUserFetch = () => {
+      
+    fetch(`https://api.github.com/users/${githubUserInputText}/repos`)
+      .then(response => response.json())
+      .then(
+        (results) => {
+          console.log(results);
+          results.map((data, index) => {
+            console.log(data.name);
+            console.log(index);
+          });
+
+          setFetchData(results);
+          reposAvailable = true;
+          console.log(reposAvailable);
+        }
+      );
+
+  };
     
   const writeData = () => {
-    fetch("http://localhost:8080/commitdetail", {
+    fetch("http://localhost:8080/repos", {
       "body": JSON.stringify(fetchData),
       "headers": {
         "Accept": "application/json",
@@ -52,33 +67,60 @@ const Test = () => {
       "method": "PUT"
     });
   };
-
-
-
   
-
-
   return (
     <>
-      <input
-        type="text"
-        value={externalInputText}
-        onChange={event => setExternalInputText(event.target.value)}
-      />
-      <button onClick={externalFetch}>External Fetch</button>
-      <input
-        type="text"
-        value={internalInputText}
-        onChange={event => setInternalInputText(event.target.value)}
-      />
-      <button onClick={internalFetch}>Internal Fetch</button>
-      <button onClick={writeData}>Write Data</button>
-      {/* <div>{JSON.stringify(fetchData)}</div> */}
       <div>
+        <ul>
+          <li>Get list of repos for a user: https://api.github.com/users/ElliotRedhead/repos?per_page=100</li>
+          <li>Get list of branches for a repo: https://api.github.com/repos/ElliotRedhead/ReactPortfolio/commits?per_page=100</li>
+        </ul>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={externalInputText}
+          onChange={event => setExternalInputText(event.target.value)}
+        />
+        <button onClick={externalFetch}>External Fetch</button>
+        <input
+          type="text"
+          value={internalInputText}
+          onChange={event => setInternalInputText(event.target.value)}
+        />
+        <button onClick={internalFetch}>Internal Fetch</button>
+        <button onClick={writeData}>Write Data</button>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={githubUserInputText}
+          onChange={event => setGithubUserInputText(event.target.value)}
+        />
+        <button onClick={githubUserFetch}>
+        Fetch Repos for User
+        </button>
+      </div>
+
+      <div>
+        {fetchData ?
+          <select>
+            {fetchData.map((data,index) => (
+              <option key={index}>{data.name}</option>
+            ))
+            }
+          </select>
+          : null
+        }
+
+      </div>
+
+
+      {/* <div>
         <ol>
           {commitsDisplay}
         </ol>
-      </div>
+      </div> */}
     </>
   );
 

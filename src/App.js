9aceea43bc1ed6312c6dc023e32fe4d0f08d62c7
ElliotRedhead <elliot.redhead@gmail.com>
2020-10-16 22:55:14 +0100

    Removed custom material UI while testing.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { ThemeProvider } from "@material-ui/core/styles";
 import GitCommits from "./components/fetch";
-import DeepPurpleTheme from "./utils/DeepPurpleTheme";
 
 
 const App = () => {
   
   return (
     <>
-      <ThemeProvider theme={DeepPurpleTheme}>
-        <Switch>
-          <Route path={"/"}>
-            <GitCommits />
-          </Route> 
-        </Switch>
-      </ThemeProvider>
+      <Switch>
+        <Route path={"/"}>
+          <GitCommits />
+        </Route> 
+      </Switch>
     </>
   );
 };

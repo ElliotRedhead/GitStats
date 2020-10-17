9e90c3ab29a8e2226172926c7d5f02bf09e92b16
ElliotRedhead <elliot.redhead@gmail.com>
2020-10-17 02:19:38 +0100

    Updates

---
+++ precache-manifest.b38c0839085363001813d771be322152.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2429fe8c6fb7d826876350ccaf030c",
+    "url": "/gitstats/index.html"
+  },
+  {
+    "revision": "ff51f3c453015570905f",
+    "url": "/gitstats/static/css/main.c55645cc.chunk.css"
+  },
+  {
+    "revision": "6e1703c816cb480ed280",
+    "url": "/gitstats/static/js/2.c1b5c492.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gitstats/static/js/2.c1b5c492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff51f3c453015570905f",
+    "url": "/gitstats/static/js/main.275e8352.chunk.js"
+  },
+  {
+    "revision": "6e6648412960990f99ed",
+    "url": "/gitstats/static/js/runtime-main.f9ce3c3a.js"
+  }
+]);

4628e0c19c87c8a896826ed3e775c4068e91660c
ElliotRedhead <elliot.redhead@gmail.com>
2020-10-18 01:36:18 +0100

    Updates

---
+++ precache-manifest.3ffaec5587cd00dc77bb073b4db0b29c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33741809bcf99e22dcfda8b7fc814bf",
+    "url": "/GitStats/index.html"
+  },
+  {
+    "revision": "500693e87b6bd99e54c1",
+    "url": "/GitStats/static/css/main.177e9cf6.chunk.css"
+  },
+  {
+    "revision": "1cdfd98e745adb3bcfbc",
+    "url": "/GitStats/static/js/2.e5e7fa01.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/GitStats/static/js/2.e5e7fa01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500693e87b6bd99e54c1",
+    "url": "/GitStats/static/js/main.620bb42c.chunk.js"
+  },
+  {
+    "revision": "9c0e1cd3b71556c6e8ba",
+    "url": "/GitStats/static/js/runtime-main.87dc1a58.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GitStats/precache-manifest.afcf5684409da1f180372cb8cecd1ae6.js"
+  "/GitStats/precache-manifest.3ffaec5587cd00dc77bb073b4db0b29c.js"
 );
 
 self.addEventListener('message', (event) => {

efd6e6b14d9d7101dd683872fa651ec99be7775a
ElliotRedhead <elliot.redhead@gmail.com>
2021-08-14 22:46:11 +0100

    Updates

---
+++ precache-manifest.22e156f3b6001cbffc80f5f5bb35b0a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d847684b7080e99765c3459e60d70402",
+    "url": "/GitStats-MiniProject/index.html"
+  },
+  {
+    "revision": "ef2c325abc75e97fda60",
+    "url": "/GitStats-MiniProject/static/css/main.a351540c.chunk.css"
+  },
+  {
+    "revision": "e9a6cde0c23da0ada74e",
+    "url": "/GitStats-MiniProject/static/js/2.8a908e3f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/GitStats-MiniProject/static/js/2.8a908e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2c325abc75e97fda60",
+    "url": "/GitStats-MiniProject/static/js/main.aef301dd.chunk.js"
+  },
+  {
+    "revision": "7c3dfc507fd04849bca0",
+    "url": "/GitStats-MiniProject/static/js/runtime-main.92ac2301.js"
+  }
+]);
